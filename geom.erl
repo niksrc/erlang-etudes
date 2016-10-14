69fd0936cea69351e29aacf818f7e93560254d0d
Nikhil Srivastava <niksrc@gmail.com>
2016-10-14 22:55:14 +0530

    2.2

--- geom.erl
+++ geom.erl
@@ -1,3 +1,8 @@
+%% @author Nikhil Srivastava niksrc@gmail.com
+%% @doc Functions to calculate area of basic shapes
+%% @copyright 2016 Nikhil Srivastava
+%% @version 0.1
+
 -module(geom).
 -export([area/2]).
 
